930c3c5913aa241c3214f1cd1895bc49bca1cb7e
ssolunna <ssolunna29@gmail.com>
2022-09-09 08:04:55 -0500

    Add grid container

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // JavaScript
 // ETCH-A-SKETCH
+
+const BODY = document.querySelector('body');
+const CONTAINER = document.createElement('div');
+CONTAINER.classList.add('container');
+BODY.appendChild(CONTAINER);
