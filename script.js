457dc8ab6a78fb34ffe0ea64a3829c7649d6cd7e
ssolunna <ssolunna29@gmail.com>
2022-09-13 08:04:23 -0500

    Add a button to change the grid size

--- script.js
+++ script.js
@@ -6,11 +6,38 @@ const CONTAINER = document.createElement('div');
 CONTAINER.classList.add('container');
 const SQUARE = document.createElement('div');
 SQUARE.classList.add('square');
-for (let i = 0; i < 256; i++) {
-  CONTAINER.appendChild(SQUARE.cloneNode());
+let squaresSize = 0;
+
+createGrid(16);
+
+function createGrid(squaresPerSide) {
+  let grid = squaresPerSide * squaresPerSide;
+  squaresSize = 100 / squaresPerSide;
+  removeOldGrid();
+  addSquares(grid);
 }
-BODY.appendChild(CONTAINER);
-const GRID = document.querySelectorAll('.square');
-GRID.forEach(square => {
-  square.onmouseenter = () => square.classList.add('passed-over');
-});
+
+function removeOldGrid() {
+  const GRID = document.querySelectorAll('.square');
+  GRID.forEach(square => square.remove());
+}
+
+function addSquares(grid) {
+  for (let i = 0; i < grid; i++) {
+    CONTAINER.appendChild(SQUARE.cloneNode());
+  }
+  BODY.appendChild(CONTAINER);
+  const GRID = document.querySelectorAll('.square');
+  GRID.forEach(square => {
+    square.style.cssText = `flex: 1 0 ${squaresSize}%;`;
+    square.onmouseenter = () => square.classList.add('passed-over');
+  });
+}
+
+const NEW_GRID_BTN = document.createElement('button');
+NEW_GRID_BTN.textContent = 'New Grid';
+CONTAINER.before(NEW_GRID_BTN);
+NEW_GRID_BTN.onclick = () => {
+  let squaresPerSide = prompt('Number of squares per side for the new grid:', '16');
+  createGrid(squaresPerSide);
+}
