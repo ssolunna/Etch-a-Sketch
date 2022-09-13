6dd22be2d247035a003366735af9b7abac73b385
ssolunna <ssolunna29@gmail.com>
2022-09-12 21:06:33 -0500

    Set up hover effect to change color of squares

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const BODY = document.querySelector('body');
 const CONTAINER = document.createElement('div');
 CONTAINER.classList.add('container');
 const SQUARE = document.createElement('div');
-SQUARE.classList.add('grid');
+SQUARE.classList.add('square');
 for (let i = 0; i < 256; i++) {
   CONTAINER.appendChild(SQUARE.cloneNode());
 }
 BODY.appendChild(CONTAINER);
+const GRID = document.querySelectorAll('.square');
+GRID.forEach(square => {
+  square.onmouseenter = () => square.classList.add('passed-over');
+});
