4c442c815b21ab8b7e80617d59ede248be7fabeb
ssolunna <ssolunna29@gmail.com>
2022-09-12 20:31:37 -0500

    Add a 16x16 grid of square divs in the container

--- script.js
+++ script.js
@@ -4,4 +4,9 @@
 const BODY = document.querySelector('body');
 const CONTAINER = document.createElement('div');
 CONTAINER.classList.add('container');
+const SQUARE = document.createElement('div');
+SQUARE.classList.add('grid');
+for (let i = 0; i < 256; i++) {
+  CONTAINER.appendChild(SQUARE.cloneNode());
+}
 BODY.appendChild(CONTAINER);
